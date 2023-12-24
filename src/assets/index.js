566a7a787bd1ac048f52827d21d3a6b8726e7c95
dsatpm <jeremiahjh@gmail.com>
2023-12-23 19:55:04 -0800

    portfolio page and dynamic rendering

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,8 @@
 import resume from './resume.pdf';
 import myFace from './ugly-mug.jpg';
+import jate from './jate.png';
+import mytholotek from './mytholotek.png';
+import blog from './blog.png';
+import social from './social-media.png';
 
-export { resume, myFace };
+export { resume, myFace, jate, mytholotek, blog, social };
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import './portfolio.scss';
+import { jate, mytholotek, blog, social } from '../../assets';
 
 const Portfolio = () => {
 	return (
@@ -6,18 +7,79 @@ const Portfolio = () => {
 			<section
 				id='portfolio'
 				className='portfolio'>
-				<h1>Portfolio</h1>
-        <div className="container">
-          <div className="img-container">
-            <img src="assets/1.png" alt="portfolio" />
-            <img src="assets/2.png" alt="portfolio" />
-            <img src="assets/3.png" alt="portfolio" />
-            <img src="assets/4.png" alt="portfolio" />
-          </div>
-        </div>
+				<h2>Portfolio</h2>
+				<div className='card-container'>
+					{portfolioItems.map((item) => (
+						<div
+							key={item.id}
+							className='card'>
+							<div className='img-content'>
+								<img
+									src={item.src}
+									alt={item.title}
+								/>
+								<div className='content'>
+									<div className='heading'>{item.title}</div>
+									{item.description}
+									<div className='card-links'>
+										<a
+											href={item.link}
+											className='btn btn-primary'>
+											Live Demo
+										</a>
+										<a
+											href={item.repo}
+											className='btn'>
+											Github
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
 			</section>
 		</>
 	);
 };
+
+const portfolioItems = [
+	{
+		id: 1,
+		title: 'J.A.T.E.',
+		src: jate,
+		link: 'https://j-a-t-e-y9ac.onrender.com/',
+		repo: 'https://github.com/dsatpm/J.A.T.E.',
+		description:
+			"J.A.T.E. is not just another text editor; it's a tool that enhances your creativity and productivity",
+	},
+	{
+		id: 2,
+		title: 'Mytholoteck',
+		src: mytholotek,
+		link: 'https://drive.google.com/file/d/16OA3Wm7n1TzKn5nqhqbO2Ti_JunAb91O/view',
+		repo: 'https://github.com/dsatpm/MYTHOLOTEK-employee-tracker',
+		description:
+			'Chart your workforce like never before, harnessing the power of mythical beings to manage your team effortlessly.',
+	},
+	{
+		id: 3,
+		title: 'Console.Blog',
+		src: blog,
+		link: 'https://console-dot-blog-9aca03c81354.herokuapp.com/',
+		repo: 'https://github.com/dsatpm/CONSOLE.BLOG-coder-blogspot',
+		description:
+			"This coder's blogspot provides a platform to share insights, experiences, and code snippets. Unleash the power of words in the coding world!",
+	},
+	{
+		id: 4,
+		title: 'Social Media API',
+		src: social,
+		link: 'https://drive.google.com/file/d/1G2YK_ZnnkUWe72x0Gh2Mnd_EmgHoohCz/view',
+		repo: 'https://github.com/dsatpm/SOCIAL-MEDIA-API',
+		description:
+			'Seamlessly integrating social interactions into your applications, this API is your gateway to creating engaging and dynamic user experiences.',
+	},
+];
 
 export default Portfolio;
